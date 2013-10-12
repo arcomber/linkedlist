f23cd2edc053945523baf48c768edfdf7743e05b
arcomber <arcomber@users.noreply.github.com>
2013-10-12 19:42:19 +0100

    Create linkedlistdata.h

---
+++ linkedlistdata.h
@@ -0,0 +1,8 @@
+/* define here data type */
+#ifndef LINKEDLISTDATA_H__
+#define LINKEDLISTDATA_H__
+
+/* example using unsigned integer type */
+typedef unsigned stackdata;
+
+#endif /* LINKEDLISTDATA_H__ */
